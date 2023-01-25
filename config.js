a63142c6cb174e2ccdf58522f32d841ee6b0e2e5
xShikuro|_ <120335556+xshikuro123@users.noreply.github.com>
2023-01-25 16:56:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,8 +16,8 @@ const _0x138100=_0x2ef9;(function(_0x11971a,_0x460754){const _0x419220=_0x2ef9,_
 
 
 global.autoTyping = false     //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false  //auto reading in gc and pm (true to on, false to off)
-global.autoReadGc = false     //auto reading in gc (true to on, false to off)
+global.autoreadpmngc = true  //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = true     //auto reading in gc (true to on, false to off)
 global.autoRecord = false     //auto recording (true to on, false to off)
 global.available = true       //auto available (true to on, false to off)
 
